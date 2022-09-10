daaceccc7e44804daa2923101d35a8492df03beb
ducky <ph.huang.ac@gmail.com>
2022-09-11 00:18:46 +0800

    學習G pass來的code加上for loop把所有txt轉檔成html檔案

--- Legistration_Yuan/Web_Parsing_3.py
+++ Legistration_Yuan/Web_Parsing_3.py
@@ -2,13 +2,7 @@ from bs4 import BeautifulSoup
 import pandas as pd
 
 
-i = 1
-#fin = open(f"Legistration_Yuan/LY2.txt") 
-# get file handle
-#data = fin.read()
 
-
-# eval() 是 把括號內的東東當作是python的code讀
 
 
 which = 0
@@ -19,7 +13,8 @@ for i in range(7):
     f = open(f"Legistration_Yuan/LY{which}.html", "w")
     
     data = handle.read()
-    data_decoded = eval(data).decode("UTF-8")
+    
+    data_decoded = eval(data).decode("UTF-8") # eval() 是 把括號內的東東當作是python的code讀
     
     print(data_decoded, file = f)
 
