deb97ec7ddf6b601588b62ac2d44f2f6c9fd9c6e
ducky <ph.huang.ac@gmail.com>
2022-09-12 02:14:10 +0800

    Functional, but the result was not clean enough. Need to figure out what went wrong.

--- Legistration_Yuan/Web_parsing_4.py
+++ Legistration_Yuan/Web_parsing_4.py
@@ -7,23 +7,20 @@ which = 0
 for i in range(7):
     which +=1
     f = open(f'LY_list_{which}.txt', 'w')
+    with open(f"Legistration_Yuan/LY{which}.html") as fp:
+        soup = BeautifulSoup(fp, 'html.parser')
+        list = soup.find("section", class_="con_data")
+        name = list.find_all("a")
 
-    response = requests.get(f"Legistration_Yuan/LY{which}.html")
-    soup = BeautifulSoup(response.text, "html.parser")
-    list = soup.find("section", class_="con_data")
-    name = list.find_all("a")
+        for j in range(len(name)):
+            print("", file=f)
+            print(name[j]["href"], file=f)
 
-    for j in range(len(name)):
-        print("", file=f)
-        print(name[j].text, file=f)
+        #    extract = requests.compat.urljoin(f"Legistration_Yuan/LY{which}.html", name[j]["href"])
+        #    open_link = requests.get(extract)
+        #    soup = BeautifulSoup(open_link.text, "html.parser")
+        #    print(extract, file=f)
 
-        extract = requests.compat.urljoin(
-        f"Legistration_Yuan/LY{which}.html", name[j]["href"])
+        #    print(name[j]["href"])
 
-        open_link = requests.get(extract)
-        soup = BeautifulSoup(open_link.text, "html.parser")
-        print(extract, file=f)
-
-        print(name[j]["href"])
-
-    f.close()
+    #f.close()
