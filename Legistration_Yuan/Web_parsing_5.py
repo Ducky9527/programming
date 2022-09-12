6beb9ac0d0efdaffe85156a5b6baef300058da5d
ducky <ph.huang.ac@gmail.com>
2022-09-12 22:10:25 +0800

    在生成dataframe時遇到一個神秘的問題： 中間的data都被略掉了...

---
+++ Legistration_Yuan/Web_parsing_5.py
@@ -0,0 +1,28 @@
+import pandas as pd
+from bs4 import BeautifulSoup
+
+
+
+
+for i in range(7):
+    
+    f = open(f'LY_list_{i+1}.csv', 'w')
+    with open(f"Legistration_Yuan/LY{i+1}.html") as fp:
+        soup = BeautifulSoup(fp, 'html.parser')
+        sections = soup.find_all("section")
+
+        for section in sections:
+            if "id" in section:
+                break
+
+        legislators = section.find_all("div", {"class": "inner"})
+        legislators_df = pd.DataFrame(columns = ['name', 'party', 'url'])
+
+
+        for legislator in legislators:
+            name = legislator.find("div", {"class": "legislatorname"}).text
+            personal_page_path = legislator.find("a")["href"]
+            party = legislator.find("img")["alt"].rstrip("徽章")
+            legislators_df.loc[len(legislators_df.index)] = [name, party, personal_page_path]
+
+    print(legislators_df, file = f)
