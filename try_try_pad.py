accb9d136cb5e52544a8407e38cbe3529dab9655
ducky <ph.huang.ac@gmail.com>
2022-12-26 18:25:44 +0800

    remove duplicatie

--- try_try_pad.py
+++ try_try_pad.py
@@ -1,3 +1,42 @@
+#This is an unsuccessful attempt with "for loop"
+
+numb = [1, 2, 1, 3, 2, 1]
+
+item_order = 1
+count = 1
+
+x = 1
+for i in numb: # problem! When this for loop began, the list contains 6 elements. SO! this for loop will execute 6 times, with i = 1, i = 2, i = 1, i = 3, i = 2, i = 1. It will not change even if in the middle of the execution some of the elements have been removed. Tricky! Tricky!
+    #1, 2, 3, 1, 2, 1
+    turn = len(numb) - item_order # I thought i could avoid the beyond the boarder issue by using "item_order". I was so wrong.
+    print(turn)
+    print(i) 
+
+    for com in range(turn): ##
+        #0, 1, 2
+        if i == numb[item_order]:
+            print(f'{count}: {i} and {numb[item_order]} are duplicate!')
+            numb.remove(numb[item_order])
+            print(f'New List: {numb}')
+            print(item_order)
+            print(" ")
+
+            count += 1
+
+        else:
+            print(f'{count}: {i} and {numb[item_order]} are not duplicate!')
+            print(f'Same List: {numb}')
+            item_order += 1
+            print(item_order)
+            print(" ")
+
+            count += 1
+ 
+
+
+#This is a successful attemp with 'while loop'
+
+
 numb = [1, 2, 3, 2, 1, 9, 3]
 
 #pop() 是指定看要remove哪個index的
